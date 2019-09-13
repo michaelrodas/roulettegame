a95aee3ec9990cc196c122b5f002ec9904419a64
michaelrodas <hollowlife7@gmail.com>
2019-09-13 13:54:05 +0200

    master initial project structure

---
+++ roulette-game/src/main/java/com/jumbo/hackathon/roulettegame/RouletteGameApplication.java
@@ -0,0 +1,13 @@
+package com.jumbo.hackathon.roulettegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouletteGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouletteGameApplication.class, args);
+	}
+
+}
